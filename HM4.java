32f69481509ffad5eaecc6d16eabfcfc1abf9268
Jake0v <s35936@pjwstk.edu.pl>
2025-10-24 11:41:45 +0200

    Add files via upload

---
+++ HM4.java
@@ -0,0 +1,94 @@
+import java.util.Scanner;
+ 
+public class HM9 {
+	 
+	 
+		void main() {
+	 
+		/* Task1
+		 Scanner scanner = new Scanner(System.in);
+		 
+		 double grade=0;
+		 
+		 System.out.println("This program calculates your grade");
+		 
+		 System.out.println("First, enter your points: ");
+		 int points = scanner.nextInt();
+		 
+		 System.out.println("Second, enter max possible points: ");
+		 int maxPoints = scanner.nextInt();
+		 
+		 double calc = (points / maxPoints) * 100;
+		 
+		 if (calc <= 50) {
+			 grade = 2;
+			 System.out.println("Your grade is 2/5");
+		 } else if (calc > 50 && calc <= 62.50) {
+			 grade = 3;
+			 System.out.println("Your grade is 3/5");
+		 } else if (calc > 62.50 && calc <= 75) {
+			 grade = 3.5;
+			 System.out.println("Your grade is 3.5/5");
+		 } else if (calc > 75 && calc <= 81.20) {
+			 grade = 4;
+			 System.out.println("Your grade is 4/5");
+		 } else if (calc > 81.20 && calc <= 87.50) {
+			 grade = 4.5;
+			 System.out.println("Your grade is 4.5/5");
+		 } else {
+			 grade = 5;
+			 System.out.println("Your grade is 5/5");
+		 }
+		 
+		 System.out.println("Your grade: " + grade);
+	     */
+		 
+		 
+		 /* Task2
+		 Scanner scanner = new Scanner(System.in);
+		 
+		 System.out.println("This program converts hexa letters to numbers");
+		 System.out.println();
+		 System.out.println("Enter a hexa letter: ");
+		 
+		 char ch = scanner.next().charAt(0);
+		 
+		 
+		 if (ch >= 'A' || ch >= 'a' && ch <= 'F' || ch <= 'f') {
+			 System.out.println(ch - 'a' + 10);
+		 } else if (ch >= '0' && ch <= '9') {
+			 System.out.println(ch - '0');
+		 } else {
+			 System.out.println("Wrong number/char");
+		 }
+		 */
+		 
+		 
+		 /* Task4
+		 IO.println("This program shows the number range [10;25] it's square and cube");
+		 IO.println();
+		 
+		 for (int i = 10; i <= 25; i++) {
+			 IO.print(i + " ");
+		 }
+		 
+		 IO.println('\n');
+		 
+		 int i = 10;
+		 while(i <= 25) {
+			 IO.print(i * i + " ");
+			 i++;
+		 }
+		 
+		 IO.println('\n');
+		 
+		 int a = 10;
+		 do {
+			IO.print(a * a * a + " ");
+			a++;
+		 } while (a <= 25);
+		 */
+	 
+	}
+	 
+ }
